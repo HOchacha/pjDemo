144f2c798be1d94ca17cc67534ed728c0e2867f9
HOchacha <outcider112@dankook.ac.kr>
2023-08-14 17:03:50 +0900

    Update SwaggerConfig.java

--- src/main/java/com/example/pjdemo/config/SwaggerConfig.java
+++ src/main/java/com/example/pjdemo/config/SwaggerConfig.java
@@ -1,14 +1,10 @@
 package com.example.pjdemo.config;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
-import io.swagger.v3.oas.annotations.info.Contact;
 import io.swagger.v3.oas.annotations.info.Info;
-import io.swagger.v3.oas.annotations.info.License;
-import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import lombok.RequiredArgsConstructor;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
